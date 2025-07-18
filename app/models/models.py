f11c6b37b784cafbc8e6cfcf3b8532520fde2d96
hong-lahee <xhfxhfldwlsk@naver.com>
2025-07-18 14:49:31 +0900

    database 연결

--- app/models/models.py
+++ app/models/models.py
@@ -5,6 +5,9 @@ class User(models.Model):
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=50)
     email = fields.CharField(max_length=100, unique=True)
+    password = fields.CharField(max_length=100)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    updated_at = fields.DatetimeField(auto_now=True)
 
     def __str__(self):
         return f"User(id={self.id}, name={self.name}, email={self.email})"
